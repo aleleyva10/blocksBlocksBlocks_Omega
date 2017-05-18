916fb281e9a2293b421a7030d175e16c1a723353
devjanaprime <dev@primeacademy.io>
2017-05-18 15:44:43 -0500

    base mode

---
+++ scripts/blocks.js
@@ -0,0 +1,48 @@
+$( document ).ready( function(){
+  console.log( 'JQ' );
+  // click handler for addBlockButton
+  $( '#addBlockButton' ).on( 'click', function(){
+    console.log( 'in addBlockButton on click' );
+    // append a block to DOM
+    var appendString = "<div class='blocky ";
+    // with a random color
+    var rando = Math.random() * 100;
+    console.log( 'rando:', rando);
+    if( rando < 10 ){
+      appendString += 'red';
+    }
+    else if( rando < 20 ){
+      appendString += 'orange';
+    }
+    else if( rando < 30 ){
+      appendString += 'yellow';
+    }
+    else if( rando < 40 ){
+      appendString += 'green';
+    }
+    else if( rando < 50 ){
+      appendString += 'blue';
+    }
+    else if( rando < 60 ){
+      appendString += 'indigo';
+    }
+    else if( rando < 70 ){
+      appendString += 'violet';
+    }
+    else {
+      appendString += 'badass';
+    }
+
+    appendString += "'></div>";
+    // append string when DONE
+    console.log( 'appending:', appendString );
+    $( '#outputDiv' ).append( appendString );
+  });
+
+  $( '#outputDiv' ).on( 'click', '.blocky', function(){
+    console.log( 'blocky class on click' );
+    // paint it black
+    $( this ).css( 'background-color', 'black' );
+  }); // end on click for blockies class on document
+
+}); //end doc ready
